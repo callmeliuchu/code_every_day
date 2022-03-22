a0e7af16e2430799a3c439d1fdbf86ad0608f89d
callmeliuchu <19931108github>
2022-03-22 15:50:33 +0800

    fix

--- digital_geometry_processing/hw1/main.py
+++ digital_geometry_processing/hw1/main.py
@@ -1,6 +1,6 @@
 from vispy import scene, io
 
-from .graph import Graph
+from digital_geometry_processing.hw1.graph import Graph
 
 canvas = scene.SceneCanvas(keys='interactive', show=True)
 view = canvas.central_widget.add_view()
@@ -10,11 +10,17 @@ verts, faces, normals, nothing = io.read_mesh("/Users/liuchu/Digital_Geometry_Pr
 mesh = scene.visuals.Mesh(vertices=verts, faces=faces, shading='smooth')
 
 view.add(mesh)
-G = Graph(verts, faces)
-points, faces1 = G.shortest_path(4234, 9894)
 
-line = scene.visuals.Line(pos=points, color=(0.8, 0.2, 0.1, 1))
-view.add(line)
+
+bar = scene.visuals.ColorBar()
+view.add(bar)
+
+
+# G = Graph(verts, faces)
+# points, faces1 = G.shortest_path(4234, 9894)
+
+# line = scene.visuals.Line(pos=points, color=(0.8, 0.2, 0.1, 1))
+# view.add(line)
 
 view.camera = scene.TurntableCamera()
 view.camera.depth_value = 10
